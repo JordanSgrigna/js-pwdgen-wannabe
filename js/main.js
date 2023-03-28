9db6c329a5530aae4a91a6af2079f2e909f60c83
JordanSgrigna <kaliwyrdiscord@gmail.com>
2023-03-28 15:11:51 +0200

    Password with division and random number

--- js/main.js
+++ js/main.js
@@ -2,6 +2,33 @@ const nome = prompt("Inserisci il tuo nome:");
 const cognome = prompt("Inserisci qui il tuo cognome:");
 const colorePreferito = prompt("Inserisci il tuo colore preferito:");
 
+/* Password con nome, cognome, colore preferito + 21 */
 let password = (nome + cognome + colorePreferito + 21);
 
-console.log(password)
+console.log(password);
+
+
+/* Password con nome, cognome, colore preferito e a/b */
+let primoNumero = prompt("Scegli un numero");
+primoNumero = parseInt(primoNumero);
+
+let secondoNumero = prompt("Scegli un altro numero");
+secondoNumero = parseInt(secondoNumero);
+
+/* Primo numero maggiore del secondo e divisibile per lo stesso */
+let quoziente = (primoNumero/secondoNumero);
+
+let passwordQuoziente = (nome + cognome + colorePreferito + quoziente);
+
+console.log(passwordQuoziente);
+
+
+
+
+/* Password con nome, cognome, colore preferito e numero random */
+let numeroRandom = Math.floor(Math.random() * 100) + 1;
+
+
+let passwordRandom = (nome + cognome + colorePreferito + numeroRandom);
+
+console.log(passwordRandom);
