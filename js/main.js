07d813206cb596b0be25fa7c2e9e728ada2ef1ad
JordanSgrigna <kaliwyrdiscord@gmail.com>
2023-03-28 14:48:02 +0200

    First task completed

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const nome = prompt("Inserisci il tuo nome:");
+const cognome = prompt("Inserisci qui il tuo cognome:");
+const colorePreferito = prompt("Inserisci il tuo colore preferito:");
+
+let password = (nome + cognome + colorePreferito + 21);
+
+console.log(password)
